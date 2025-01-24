61096b9d5f7726cc0f20201d9f192aa7cfe3053f
Hakim <hakimkavuma99@gmail.com>
2025-01-24 22:17:02 +0300

    Add test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+first_number = 8
+second_number = 2
+third_number = 30
+
+result_of_e xponetion = first_number ** second_number
+division_with_remainder = third_number / first_number
+division_without_remainder = third_number // first_number
+
+sum_of_all = result_of_exponetion + division_with_remainder + division_without_remainder
+
+print(sum_of_all)
+print(type(sum_of_all))
