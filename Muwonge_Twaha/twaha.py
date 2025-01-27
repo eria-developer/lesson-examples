86a8287be3018ebc014cdc84ba8aa051269e8387
johnma468 <bromc740@gmail.com>
2025-01-27 20:50:26 +0300

    Feature:i have added a math page

---
+++ Muwonge_Twaha/twaha.py
@@ -0,0 +1,27 @@
+#python pro-gram to slove the puzzle
+
+bass_number = 7
+expotential_number = 5
+
+power_result = bass_number**expotential_number
+
+print(power_result)
+
+#Large number
+large_number = 90
+
+#q divison
+quotient_division = large_number//bass_number
+
+print(quotient_division)
+
+#Exact division
+exact_division = large_number/bass_number
+
+print(exact_division)
+
+final_value = power_result + quotient_division + exact_division
+
+print(final_value)
+print(type(final_value))
+
