950b7a97cd2dba7da192bf219f72e31be3f9f73c
allan <lutaayallan100k@gmail.com>
2025-01-27 15:09:04 +0300

    added allan.py file

---
+++ allan.py
@@ -0,0 +1,11 @@
+print("hello world")
+my_name="Allan"
+print(my_name)
+print("fahad is misbehaving","because","is a planned kid")
+my_nAme="LUTAAYA"
+my_Name="ALLAN"
+print(my_nAme,my_Name)
+height= 5.6
+print(type(height))
+height=5.6
+my_varieble_type=(type(height))
