6d38ac34a65374fde4a5d63b2f2f4e926945a4ce
khangu1 <khangumelissa@gmail.com>
2025-01-27 13:10:56 +0300

    FEATURE: Khangu Melissa has added a new math puzzle

---
+++ Khangu Melissa/math_puzzle.py
@@ -0,0 +1,25 @@
+#first number
+base=2
+
+#second number
+exponent=3
+
+#exponential calculation
+exponential_value=base**exponent
+
+#third number
+third_value=24
+
+#quotient with no remainder
+quotient=third_value//base
+
+#exact_division_value
+exact_division_value=third_value/base
+
+#total
+total=exponential_value+quotient+exact_division_value
+
+#output
+print(total)
+print(type(total))
+
