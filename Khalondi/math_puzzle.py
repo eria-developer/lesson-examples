b140affbc426fbabbe7875f8c702e507eb574ccd
Khalondi <khalondivanessa@gmail.com>
2025-01-27 13:52:21 +0300

    FEATURE: I have added a math puzzle file

---
+++ Khalondi/math_puzzle.py
@@ -0,0 +1,13 @@
+premiere_no=6
+deuxieme_no=10
+multiple= premiere_no**deuxieme_no
+#print(multiple)
+plus_grand_no=15
+quotient= plus_grand_no/premiere_no
+#print (plus_grand_no)
+exact_quotient= plus_grand_no//premiere_no
+#print(exact_quotient)
+ajout_dautre= multiple + quotient + exact_quotient 
+print(ajout_dautre)
+print (type(ajout_dautre))
+
