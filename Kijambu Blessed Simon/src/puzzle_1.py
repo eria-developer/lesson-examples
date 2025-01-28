3cd2894476f384dbddf21188fbddedcb9a317ca0
blexiestar <blessedkijambu@gmail.com>
2025-01-28 19:02:57 +0300

    FEATURE: I have added a puzzle file

---
+++ Kijambu Blessed Simon/src/puzzle_1.py
@@ -0,0 +1,26 @@
+# solving the math puzzle using python program
+base_number = 3
+exponential_number = 5
+
+# power result
+power_outcome = 3**5
+print(power_outcome)
+
+# working with another number
+third_number = 20
+
+# quotient division 
+quotient_division = third_number//base_number
+print(quotient_division) 
+
+# exact division
+exact_division = third_number/base_number
+print(exact_division)
+
+# finding the final number
+final_number = exponential_number + quotient_division + exact_division
+print(final_number)
+
+# type of final value
+print(type(final_number))  # the type is a float
+
