8ac27b75be824bbf174fb8a6c4f14ae85ce16a9d
2002-sketch <magezijoshua05@gmail.com>
2025-01-29 22:25:34 +0300

    submission of assignment

---
+++ joshua1 puzzle.py
@@ -0,0 +1,9 @@
+base=3 
+exponent =4
+power_result=  base**exponent
+larger_number=10
+quotient= larger_number//base
+exact_division =larger_number/base
+final_value = power_result + quotient + exact_division
+print(final_value)
+print(type(final_value))
